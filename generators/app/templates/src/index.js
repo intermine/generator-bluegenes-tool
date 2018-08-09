5449c79d60c8a4b97ad3910b04577448ece0fb1f
Yo Yehudi <yo@dhcp-10-248-143-164.eduroam.wireless.private.cam.ac.uk>
2018-08-09 16:20:20 +0100

    add index.js with example content

---
+++ generators/app/templates/src/index.js
@@ -0,0 +1,19 @@
+//add any imports if needed, or write your script directly in this file.
+//import SomePackage from "PackageName";
+
+//make sure to export main, with the signature
+export function main (el, service, imEntity, state, config) {
+    // sample code here to convert the provided intermine object (e.g. perhaps
+    // an id) into an identifier the tool expects. e.g.:
+
+/****** Example *******
+    //protVista expects an accession, so convert intermine id to accession
+
+    var columnToConvert = config.columnMapping[imEntity.class][imEntity.format];
+    var accession = new imjs.Service(service)
+        .findById(imEntity.class, imEntity.value)
+        .then(function(response) {
+        //put some code here to initialise your tool.
+    });
+******* End Example *******/
+}
