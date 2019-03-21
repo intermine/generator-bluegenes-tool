962893017f233e701f1258a8182fe88521ceafa5
akshat <akshatbhargavadeepa@gmail.com>
2019-03-21 20:15:32 +0530

    fixed bug - `dev/serve.js` now gets copied when generating project. Added the same path to test file.

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,14 +2,14 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 
 module.exports = class extends Generator {
   initializing() {
     this.composeWith(require.resolve('generator-license'), {
-    defaultLicense: 'MIT' // (optional) Select a default license
-  })
-}
+      defaultLicense: 'MIT' // (optional) Select a default license
+    });
+  }
 
   prompting() {
     // Have Yeoman greet the user.
@@ -17,22 +17,26 @@ module.exports = class extends Generator {
       yosay(`Welcome to the laudable ${chalk.red('generator-bluegenes-tool')} generator!`)
     );
 
-    const prompts = [{
+    const prompts = [
+      {
         type: 'input',
         name: 'toolNameCljs',
-        message: 'What shall we name your project? This is a computer name with no spaces or special characters.',
+        message:
+          'What shall we name your project? This is a computer name with no spaces or special characters.',
         default: 'bluegenesToolNameHere'
       },
       {
-          type: 'input',
-          name: 'toolNameNpm',
-          message: 'NPM package name? This is a computer name with no capital letters or special characters apart from the - dash.',
-          default: 'bluegenes-tool-name-here'
-        },
+        type: 'input',
+        name: 'toolNameNpm',
+        message:
+          'NPM package name? This is a computer name with no capital letters or special characters apart from the - dash.',
+        default: 'bluegenes-tool-name-here'
+      },
       {
         type: 'input',
         name: 'toolNameHuman',
-        message: 'Thanks! Now, give me a human name for the project - e.g. "Protein Feature Viewer"',
+        message:
+          'Thanks! Now, give me a human name for the project - e.g. "Protein Feature Viewer"',
         default: 'New Bluegenes Tool'
       },
       // {
@@ -44,7 +48,8 @@ module.exports = class extends Generator {
       {
         type: 'input',
         name: 'classes',
-        message: 'Fabulous. Which report pages do you expect this tool to work for, e.g. "Gene" or "Protein"? Separate with commas and put * for all.',
+        message:
+          'Fabulous. Which report pages do you expect this tool to work for, e.g. "Gene" or "Protein"? Separate with commas and put * for all.',
         default: 'Gene'
       },
       {
@@ -80,24 +85,23 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('demo.html'),
-      this.destinationPath('demo.html'), {
-        title: this.props.toolNameHuman,
-        toolNameCljs: this.props.toolNameCljs,
-        mineUrl: 'http://www.humanmine.org/human'
-      }
-    );
+    this.fs.copyTpl(this.templatePath('demo.html'), this.destinationPath('demo.html'), {
+      title: this.props.toolNameHuman,
+      toolNameCljs: this.props.toolNameCljs,
+      mineUrl: 'http://www.humanmine.org/human'
+    });
     this.fs.copyTpl(
       this.templatePath('webpack.config.js'),
-      this.destinationPath('webpack.config.js'), {
+      this.destinationPath('webpack.config.js'),
+      {
         toolNameCljs: this.props.toolNameCljs
       }
     );
 
     this.fs.copyTpl(
       this.templatePath('config.json'),
-      this.destinationPath('config.json'), {
+      this.destinationPath('config.json'),
+      {
         accepts: JSON.stringify(this.props.accepts),
         toolNameHuman: this.props.toolNameHuman,
         toolNameCljs: this.props.toolNameCljs,
@@ -105,50 +109,58 @@ module.exports = class extends Generator {
       }
     );
 
-  this.fs.copyTpl(
-    this.templatePath('package.json'),
-    this.destinationPath('package.json'), {
-      author: this.props.author,
-      toolNameNpm: this.props.toolNameNpm,
-      licence: this.props.licence
-    }
-  );
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      {
+        author: this.props.author,
+        toolNameNpm: this.props.toolNameNpm,
+        licence: this.props.licence
+      }
+    );
 
-  this.fs.copyTpl(
-    this.templatePath('README.md'),
-    this.destinationPath('README.md'), {
+    this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('README.md'), {
       author: this.props.author,
       toolNameHuman: this.props.toolNameHuman,
       toolNameNpm: this.props.toolNameNpm,
       toolNameCljs: this.props.toolNameCljs,
       licence: this.props.licence
-    }
-  );
+    });
 
-  this.fs.copyTpl(
-    this.templatePath('src/style.less'),
-    this.destinationPath('src/style.less'), {
-      toolNameCljs: this.props.toolNameCljs
-    }
-  );
+    this.fs.copyTpl(
+      this.templatePath('src/style.less'),
+      this.destinationPath('src/style.less'),
+      {
+        toolNameCljs: this.props.toolNameCljs
+      }
+    );
 
-  this.fs.copyTpl(
-    this.templatePath('src/index.js'),
-    this.destinationPath('src/index.js'), {}
-  );
-}
-install() {
-  this.installDependencies({
+    this.fs.copyTpl(
+      this.templatePath('src/index.js'),
+      this.destinationPath('src/index.js'),
+      {}
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('dev/serve.js'),
+      this.destinationPath('dev/serve.js'),
+      {}
+    );
+  }
+
+  install() {
+    this.installDependencies({
       npm: true,
       bower: false,
-      yarn: false});
-}
+      yarn: false
+    });
+  }
 };
 
 function stringToMultiValue(values) {
-  //split and trim values. Return pseudo-aray.
-  var vals = values.split(",");
-  //no more whitespace, please
-  vals = vals.map(val => val.replace(/\s+/g, ''))
+  // Split and trim values. Return pseudo-aray.
+  var vals = values.split(',');
+  // No more whitespace, please
+  vals = vals.map(val => val.replace(/\s+/g, ''));
   return JSON.stringify(vals);
 }
