7809d72157448fa092b36ec97df97191f97905cd
akshat <akshatbhargavadeepa@gmail.com>
2019-05-13 13:55:58 +0530

    wrote a mock test for index.js

---
+++ generators/app/templates/tests/index.test.js
@@ -0,0 +1,11 @@
+const main = require('../src').main;
+
+// Example
+describe('main', () => {
+	test('should throw error when called with wrong signature', () => {
+		expect(() => {
+      // testing with all falsy values
+			main('', 0, null, undefined, []);
+		}).toThrowError('Call main with correct signature');
+	});
+});
