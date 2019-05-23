4608888fbcf07507200d586e25dcc89bfe24831b
akshat <akshatbhargavadeepa@gmail.com>
2019-05-23 22:47:08 +0530

    single command  does everything now, socketio integrated for automatic browser reloading

--- generators/app/templates/dev/serve.js
+++ generators/app/templates/dev/serve.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var serveStatic = require('serve-static');
+var chokidar = require('chokidar');
+var spawnCommand = require('child_process').spawnSync;
 var port = 3456;
 
+var io = require('socket.io')(3457);
 var app = express();
+
+var watcher = chokidar.watch('src');
+watcher.on('all', (eventName, path) => {
+	if (eventName === 'addDir') {
+		console.log('Building files...');
+		spawnCommand('npm', ['run', 'less']);
+		spawnCommand('npm', ['run', 'build']);
+		console.log('Files built!');
+	} else if (eventName === 'add') return;
+	else if (eventName === 'change') {
+		console.log('Building files...');
+		if (path.search('.less') !== -1) {
+			spawnCommand('npm', ['run', 'less']);
+		} else {
+			spawnCommand('npm', ['run', 'build']);
+		}
+		io.emit('reload');
+		console.log('Reloaded!');
+	}
+});
 
 app.use(serveStatic('./', {
   'index': ['demo.html']
