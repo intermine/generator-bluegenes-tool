70dc05a82fffa1f23cad0ab7a653fa4865fedf6f
Yo Yehudi <yo@intermine.org>
2019-05-28 14:54:05 +0100

    really add debug comments

--- setup-bluegenes.sh
+++ setup-bluegenes.sh
@@ -1,17 +1,22 @@
 #tell bluegenes which intermine to query
 export BLUEGENES_DEFAULT_SERVICE_ROOT=$TESTMODEL_URL
 
+echo "#--- installing lein"
 #download and install lein
 wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -O /tmp/lein
 chmod +x /tmp/lein
 export PATH=$PATH:/tmp/lein
 #start server
+
+echo "#--- starting bluegenes server"
 lein run &
 # install cypress dependencies
 sudo apt-get install xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 -y
 # setup dependencies
 npm install
-# Build CSS
+
+echo "#--- building css"
 lein less once
-# ensure a minified build completes without error
+
+echo "#--- building cljs into js, prod minified"
 lein cljsbuild once min
