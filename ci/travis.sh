97680b4ddd41f4d25fccfeec2706a83ea8e8229c
Yo Yehudi <yo@intermine.org>
2019-05-29 14:37:27 +0100

    trying to set up bluegenes tools and cypress test

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,5 +1,7 @@
-# Pipes the word "Gene" to all of the yeoman responses
+# Generate a new tool.
+# ------------
+# Pipes the word "Employee" to all of the yeoman responses
 # ensuring we have some default input for the tool
 # as well as a valid object class for which pages to
 # show this on.
-yes "Gene" | yo @intermine/bluegenes-tool
+yes "Employee" | yo @intermine/bluegenes-tool
--- setup-bluegenes.sh
+++ setup-bluegenes.sh
@@ -1,5 +1,6 @@
 #tell bluegenes which intermine to query
 export BLUEGENES_DEFAULT_SERVICE_ROOT=$TESTMODEL_URL
+export BLUEGENES_TOOL_PATH="bluegenes/tools/node_modules"
 
 echo "#--- installing lein"
 #download and install lein
@@ -13,8 +14,9 @@ git clone --single-branch --branch 'dev' --depth 1 https://github.com/intermine/
 cd bluegenes
 npm install
 
+cd tools
+mv ../../ci/package.json package.json -f
+
 echo "#--- starting bluegenes server"
 lein prod
-# install cypress dependencies
-sudo apt-get install xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 -y
 # setup dependencies
