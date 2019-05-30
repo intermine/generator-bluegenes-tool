87b24b704a449900c99496c234a05087f0027392
Yo Yehudi <yo@intermine.org>
2019-05-30 12:02:15 +0100

    trying to get prompts working for the generator

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,14 +1,19 @@
 echo "#--- generating sample tool"
 npm link # sets this repo up as a global dependency
+
+mkdir sample_tool
+cd sample_tool
+
 # print a bunch of dummy responses to the generator.
 # We're using the test model which is non-biological
 # and testing on the Employee report page.
-printf 'Employee\nEmployee\nEmployee\nEmployee\n\n\nEmployee\n\n\n\n' | yo @intermine/bluegenes-tool
+while true; do echo "Employee\n"; sleep 0.1s; done | yo @intermine/bluegenes-tool
 
 npm run build
 
 echo "#--- copy tool to bluegenes tool dir"
-mv Employee ../bluegenes/tools/node_modules/@intermine/Employee
+cd ..
+mv sample_tool ../bluegenes/tools/node_modules/@intermine/Employee
 
 echo "#--- Run UI test"
 npx cypress run
