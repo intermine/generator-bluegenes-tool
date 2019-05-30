452eed13a500e2a3e4233687dc260048d535cf2a
Yo Yehudi <yo@intermine.org>
2019-05-30 12:14:29 +0100

    try to make the generator answers work more slowly and go to the right dir

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,17 +1,25 @@
 echo "#--- generating sample tool"
 npm link # sets this repo up as a global dependency
 
+echo "#---- new folder for sample tool"
+
 mkdir sample_tool
 cd sample_tool
 
+echo "#---- current dir is:"
+
+pwd
+
+echo "#---- run yeoman generator"
 # print a bunch of dummy responses to the generator.
 # We're using the test model which is non-biological
 # and testing on the Employee report page.
 while true; do echo "Employee\n"; sleep 0.1s; done | yo @intermine/bluegenes-tool
 
+echo "#---- build tool"
 npm run build
 
-echo "#--- copy tool to bluegenes tool dir"
+echo "#---- copy tool to bluegenes tool dir"
 cd ..
 mv sample_tool ../bluegenes/tools/node_modules/@intermine/Employee
 
