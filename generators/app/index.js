13a7e0d1af7b8dbaf56fbe3970ad360a7e067cb9
uosl <regen@anche.no>
2019-09-16 15:11:32 +0100

    Yikes - NPM still messes with .gitignore, so we have to rename it
    Also bump versino to 1.3.3.

--- generators/app/index.js
+++ generators/app/index.js
@@ -186,7 +186,7 @@ module.exports = class extends Generator {
     );
 
     this.fs.copyTpl(
-      this.templatePath('.gitignore'),
+      this.templatePath('gitignore'),
       this.destinationPath('.gitignore'),
       {}
     );
